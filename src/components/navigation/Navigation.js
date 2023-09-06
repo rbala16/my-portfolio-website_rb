bc5059f98ec8ed84bb2780b54aeb2366b6671db4
Rajni bala <balatamna16@gmail.com>
2023-09-06 17:31:06 -0400

    finished with header and nav components

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -8,9 +8,19 @@ const Navigation = () => {
 const [click,setClick]= useState(false);
 const  handleClick = () => setClick(!click);
 
+const [color,setColor]= useState(false);
+const changeColor = ()=>{
+  if(window.scrollY>=100){
+    setColor(true);
+  }
+  else{
+    setColor(false);
+  }
+  }
+ window.addEventListener('scroll',changeColor);
 
   return (
-    <nav className='header'>
+    <nav className={color ? 'header header-bg': 'header'}>
       <h1>Portfolio</h1>
 <ul className={click ? 'nav-menu active': 'nav-menu'}>
 
