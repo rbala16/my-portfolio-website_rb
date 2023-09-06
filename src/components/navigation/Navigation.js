c87bba760d4f4aa38a1c3f45326816b3d1c2b7c7
Rajni bala <balatamna16@gmail.com>
2023-09-05 23:05:47 -0400

    navbar styling

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navigation.css';
+import {FaBars, FaTimes} from "react-icons/fa";
+
 
 const Navigation = () => {
+
+const [click,setClick]= useState(false);
+const  handleClick = () => setClick(!click);
+
+
   return (
     <nav className='header'>
       <h1>Portfolio</h1>
-<ul className='nav-menu'>
+<ul className={click ? 'nav-menu active': 'nav-menu'}>
 
-  <li><a class="active" href="#home">Home</a></li>
+  <li><a  href="#home">Home</a></li>
   <li><a href="#news">News</a></li>
   <li><a href="#contact">Contact</a></li>
   <li><a href="#about">About</a></li>
 </ul>
+<div className='hamburger' 
+onClick={handleClick}>
+  {click ? (
+     <FaTimes size={20} style={{color: '#fff'}}
+  />
+  ): (
+  <FaBars size={20} style={{color: '#fff'}}
+  />
+  )}
+ 
+</div>
     </nav>
   )
 }
