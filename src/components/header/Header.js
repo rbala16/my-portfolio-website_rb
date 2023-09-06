13a260b1b9a3c0786c9fabf7bbdba7cb78546b73
Rajni bala <balatamna16@gmail.com>
2023-09-06 11:31:09 -0400

    home page created

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './header.css';
 import CTA from './CTA';
 
+
 const Header = () => {
   return (
+    
 <header>
   <div className="container header">
   <h5>Hello I'm</h5>
