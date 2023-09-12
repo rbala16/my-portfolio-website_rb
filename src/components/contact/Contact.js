646c572b28eddbbbf1e27f14e232e127db4be00f
Rajni bala <balatamna16@gmail.com>
2023-09-11 21:40:34 -0400

    contact page created

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import "./contact.css";
-import HeaderImg from '../headerImg/headerImg';
+import ContactImg from '../../assets/background.jpeg';
 
 const Contact = () => {
   return (
-<HeaderImg heading='CONTACT.' text='Lets Chat'/>
+   
+    <div className="container">
+  <div className='container-content'>
+    <h2>Contact Us</h2>
+    <p>Swing by for a cup of coffee, or leave us a message:</p>
+  </div>
+  <div className="row">
+    <div className="column">
+      <img src={ContactImg} alt='contact-img'/>
+    </div>
+    <div className="column">
+    <form action="/" method="post" className="form_details">
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+        <label for="lname">Last Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+        <label for="subject">Subject</label>
+        <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+        <input type="submit" value="Submit"/>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+
   )
 }
 
