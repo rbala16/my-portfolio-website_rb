12f1a0903c2f28d1c8cb1973880445e534928b35
Rajni bala <balatamna16@gmail.com>
2023-09-12 16:20:27 -0400

    Contact page finished

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Aboutpage from '../components/about/About';
+import Navigation from '../components/navigation/Navigation';
+import Footer from '../components/footer/Footer';
 
 const About = () => {
   return (
     <div>
+     
+      <Navigation/>
       <Aboutpage/>
+      <Footer/>
     </div>
   )
 }
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Projectpage from '../components/project/Project';
+import Footer from '../components/footer/Footer';
+import Navigation from '../components/navigation/Navigation';
 
 const Project = () => {
   return (
     <div>
+      <Navigation/>
       <Projectpage/>
+      <Footer/>
     </div>
   )
 }
